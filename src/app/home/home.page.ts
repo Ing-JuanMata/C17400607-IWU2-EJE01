d90cc8496ccb2b90be7287bd4afd36bbeb37b847
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2023-03-16 08:32:17 -0700

    FIREBASE HOSTING

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public products = [
+    { name: 'Producto 1', price: 20, type: 'Abarrotes' },
+    { name: 'Jabon Zote', price: 30, type: 'Limpieza' },
+    { name: 'Croquetas 50kg', price: 400, type: 'Mascotas' },
+  ];
 
   constructor() {}
 
+  getColor(type: string): string {
+    switch (type) {
+      case 'Abarrotes':
+        return 'primary';
+      case 'Limpieza':
+        return 'success';
+      case 'Mascotas':
+        return 'warning';
+      default:
+        return 'primary';
+    }
+  }
 }
